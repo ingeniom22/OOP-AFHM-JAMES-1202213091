1e4aee6c845c68015de1f6ece8f528a53e75d1a6
ingeniom22 <jamesmichael0444@gmail.com>
2022-12-23 15:42:57 +0700

    commit tp3

---
+++ TPMODUL3_JAMES/src/Waiters.java
@@ -0,0 +1,45 @@
+public class Waiters implements Runnable {
+
+    private final int orderQty;
+    private final int customerID;
+    static int foodPrice = 25000;
+
+    public Waiters(int orderQty, int customerID) {
+        this.orderQty = orderQty;
+        this.customerID = customerID;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            getFood();
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void orderInfo() {
+        System.out.println("==========================================================");
+        System.out.println("Customer ID: " + this.customerID);
+        System.out.println("Number of Food: " + this.orderQty);
+        System.out.println("Total Price: " + this.orderQty * foodPrice);
+        System.out.println("==========================================================");
+    }
+
+    public void getFood() {
+        synchronized (Restaurant.getLock()) {
+            System.out.println("Waiter: Food is ready to deliver");
+
+            if (Restaurant.getFoodNumber() == this.orderQty + 1) {
+                orderInfo();
+                System.exit(0);
+            }
+            Restaurant.getLock().notifyAll();
+            System.out.println("Waiter: Tell the resturant to make another food\n");
+
+        }
+    }
+}
